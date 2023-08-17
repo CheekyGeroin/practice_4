56c1fc31550be4bde34eefba5b953e271991c010
CheekyGeroin <97744740+CheekyGeroin@users.noreply.github.com>
2023-08-17 12:35:38 +0000

    Deploying to gh-pages from @ CheekyGeroin/practice_4@b37cb6b87223cce9be48d7e7803eb491cec0aeec 🚀

---
+++ 01-color-switcher.6062212c.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null;t.stopBtn.disabled=!0;const n=()=>{clearInterval(e),t.stopBtn.disabled=!0,t.startBtn.disabled=!1};t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1,t.stopBtn.addEventListener("click",n)}));
+//# sourceMappingURL=01-color-switcher.6062212c.js.map
